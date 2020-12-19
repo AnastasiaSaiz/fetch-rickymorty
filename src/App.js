56bd0bf43f2836e772f06044de121b9db14bbf04
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-19 18:59:33 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [next, setNext] = useState("");
+  const [prev, setPrev] = useState("");
+  const [url, setUrl] = useState("https://rickandmortyapi.com/api/character/")
+
+
+  useEffect(() => {
+    fetch(url).then(res => res.json()).then(res => {
+      setNext(res.info.next)
+      setPrev(res.info.prev)
+      setData(res.results)
+    });
+  }, [url]);
+
+  const paginaSiguiente = () => {
+    if (next !== null) {
+      setUrl(next);
+    }
+  };
+  const paginaAnterior = () => {
+    if (prev !== null) {
+      setUrl(prev);
+    }
+  }
+
+  const mostrarPersonajes = data.map((personaje => {
+    return (
+      <div>
+        <img src={personaje.image} />
+        <h4>{personaje.name}</h4>
+      </div>
+    )
+  }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {mostrarPersonajes}
+      <button onClick={paginaAnterior}>Anterior</button>
+      <button onClick={paginaSiguiente}>Siguiente</button>
+
+    </>
+  )
 }
 
 export default App;
